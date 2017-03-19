3ff084ef63a9124897bf38a79e23adde593bcd9c
Alan Gutierrez <alan@prettyrobots.com>
2017-03-19 10:54:49 -0500

    Implementation.

---
+++ cubbyhole.js
@@ -0,0 +1,22 @@
+var Vivifyer = require('vivifyer')
+var Signal = require('signal')
+
+function Cubbyhole () {
+    this._signals = new Vivifyer(function () { return new Signal })
+}
+
+Cubbyhole.prototype.get = function (key, callback) {
+    this._signals.get(key).wait(callback)
+}
+
+Cubbyhole.prototype.set = function (key) {
+    var vargs = Array.prototype.slice.call(arguments, 1)
+    var signal = this._signals.get(key)
+    signal.unlatch.apply(signal, vargs)
+}
+
+Cubbyhole.prototype.remove = function (key) {
+    delete this._signals.map[key]
+}
+
+module.exports = Cubbyhole
--- t/cubbyhole.t.js
+++ t/cubbyhole.t.js
@@ -1,6 +1,12 @@
-require('proof')(1, prove)
+require('proof')(2, prove)
 
 function prove (assert) {
     var Cubbyhole = require('../cubbyhole')
-    assert(Cubbyhole, 'require')
+    var cubbyhole = new Cubbyhole
+    cubbyhole.get('x', function (error, result) {
+        assert(error, null, 'no error')
+        assert(result, 1, 'result')
+        cubbyhole.remove('x')
+    })
+    cubbyhole.set('x', null, 1)
 }
