52f4f10c9b8e201b898fec662d6f0ea1038e0372
Alan Gutierrez <alan@prettyrobots.com>
2018-10-16 11:53:52 -0500

    Rename `assert` to `okay`.

--- t/cubbyhole.t.js
+++ t/cubbyhole.t.js
@@ -1,12 +1,12 @@
 require('proof')(3, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Cubbyhole = require('../cubbyhole')
     var cubbyhole = new Cubbyhole
     cubbyhole.wait('x', function (error, result) {
-        assert(error, null, 'no error')
-        assert(result, 1, 'result')
-        assert(cubbyhole.keys(), [ 'x' ], 'keys')
+        okay(error, null, 'no error')
+        okay(result, 1, 'result')
+        okay(cubbyhole.keys(), [ 'x' ], 'keys')
         cubbyhole.remove('x')
     })
     cubbyhole.set('x', null, 1)
