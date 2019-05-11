71ebe5e1a48da1e32a37fd829398aefce2fa9e46
Alan Gutierrez <alan@prettyrobots.com>
2019-05-10 21:52:09 -0500

    ES6 Cubbyhole.

---
+++ es6.js
@@ -0,0 +1,62 @@
+const Vivifyer = require('vivifyer')
+
+class Latch {
+    constructor () {
+        this.promise = new Promise((resolve, reject) => {
+            this.resolve = resolve
+            this.reject = reject
+        })
+        this.unlatched = false
+    }
+
+    unlatch (value) {
+        if (!this.unlatched) {
+            this.unlatched = true
+            if (value instanceof Error) {
+                this.reject.call(null, value)
+            } else {
+                this.resolve.call(null, value)
+            }
+        }
+    }
+}
+
+class Cubbyhole {
+    constructor () {
+        this._latches = new Vivifyer(() => new Latch)
+        this._terminator = null
+        this.destroyed = false
+    }
+
+    get keys () {
+        return Object.keys(this._latches.map)
+    }
+
+    async get(key) {
+        const latch = this._terminator || this._latches.get(key)
+        return await latch.promise
+    }
+
+    set (key, value) {
+        if (!this.destroyed) {
+            this._latches.get(key).unlatch(value)
+        }
+    }
+
+    remove (key) {
+        delete this._latches.map[key]
+    }
+
+    destroy () {
+        const value = arguments.length == 0 ? null : arguments[0]
+        this.destroyed = true
+        this._terminator = new Latch()
+        this._terminator.unlatch(value)
+        for (let key in this._latches.map) {
+            this._latches.map[key].unlatch(value)
+            delete this._latches.map[key]
+        }
+    }
+}
+
+module.exports = Cubbyhole
---
+++ test/cubbyhole.test.js
@@ -0,0 +1,44 @@
+describe('cubbyhole', async () => {
+    const Cubbyhole = require('../es6')
+    const assert = require('assert')
+    it('can be constructed', () => {
+        const cubbyhole = new Cubbyhole
+        cubbyhole.destroy()
+    })
+    it('can get a value', async () => {
+        const cubbyhole = new Cubbyhole
+        const got = cubbyhole.get('x')
+        cubbyhole.set('x', 1)
+        assert.equal(await got, 1, 'got')
+        assert.deepStrictEqual(cubbyhole.keys, [ 'x' ], 'keys')
+    })
+    it('can remove a key', async () => {
+        const cubbyhole = new Cubbyhole
+        cubbyhole.set('x', 1)
+        assert.deepStrictEqual(cubbyhole.keys, [ 'x' ], 'keys')
+        cubbyhole.remove('x')
+        assert.deepStrictEqual(cubbyhole.keys, [], 'removed')
+    })
+    it('can reject', async () => {
+        const test = []
+        const cubbyhole = new Cubbyhole
+        cubbyhole.set('x', new Error('reject'))
+        try {
+            await cubbyhole.get('x')
+        } catch (error) {
+            test.push(error.message)
+        }
+        assert.deepStrictEqual(test, [ 'reject' ], 'rejected')
+    })
+    it('can be destroyed', async () => {
+        const cubbyhole = new Cubbyhole
+        cubbyhole.set('x', 1)
+        assert.deepStrictEqual(cubbyhole.keys, [ 'x' ], 'keys')
+        const promise = cubbyhole.get('y')
+        cubbyhole.destroy(2)
+        cubbyhole.set('x', 1)
+        assert.deepStrictEqual(cubbyhole.keys, [], 'removed')
+        assert.equal(await promise, 2, 'destroyed get')
+        assert.equal(await cubbyhole.get('z'), 2, 'get after destroyed')
+    })
+})
