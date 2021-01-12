7de15c9043e5e9c12b22209dd6f8f50151d672a7
Alan Gutierrez <alan@prettyrobots.com>
2021-01-12 03:50:43 -0600

    Replace internal future with Perhaps.

--- cubbyhole.js
+++ cubbyhole.js
@@ -1,104 +1,79 @@
+const assert = require('assert')
+
 // Map that generates an entry if missing.
+const Future = require('perhaps')
 const Vivifyer = require('vivifyer')
 
-// An implementation of a future. Not using `prospective/future` because I need
-// to swallow any rejections and `prospective/future` doesn't do that. We want
-// the user to be able to `get` a value from the `Cubbyhole` at their leisure.
-// We do not want to obligate them to handle rejections for all the possible
-// keys in the `Cubbyhole`.
-
 //
-class Latch {
-    // Construct a `Latch`. Ensure that we'll not raise an unhandled rejection
-    // if no one is currently waiting on the latch.
-    constructor () {
-        // Mark as currently unlatched.
-        this.unlatched = false
-        // Advertise a `Promise` that contains the value of the `Latch`.
-        this.promise = new Promise((resolve, reject) => {
-            this.resolve = resolve
-            this.reject = reject
-        })
-        // Ensure that there are no unhandled rejections.
-        this.promise.catch(() => {})
-    }
-
-    // Unlatch the `Latch` with the given value. If the `value` is an `Error`,
-    // reject the promise with the `Error`, otherwise resolve the `Promise` with
-    // the `value`.
-    unlatch (value) {
-        if (!this.unlatched) {
-            this.unlatched = true
-            if (value instanceof Error) {
-                this.reject.call(null, value)
-            } else {
-                this.resolve.call(null, value)
-            }
-        }
-    }
-}
-
 class Cubbyhole {
     // Construct an empty `Cubbyhole`.
     constructor () {
-        this._latches = new Vivifyer(() => new Latch)
+        this._futures = new Vivifyer(() => new Future)
         this._terminator = null
         // When true, the `Cubbyhole` has been destroyed and all keys will
         // resolve with an error.
         this.destroyed = false
     }
 
-    // **TODO** Maybe rename to `unresolved`.
-    // An array of all the unresolved entries in the `Cubbyhole`.
-    get latched () {
-        const latched = []
-        for (const key in this._latches.map) {
-            if (!this._latches.map[key].unlatched) {
-                latched.push(key)
+    // An array of all the unfulfilled entries in the `Cubbyhole`.
+    get unfulfilled () {
+        const unfulfilled = []
+        for (const key in this._futures.map) {
+            if (! this._futures.map[key].fulfilled) {
+                unfulfilled.push(key)
             }
         }
-        return latched
+        return unfulfilled
     }
 
     // An array of all the keys in the `Cubbyhole`.
     get keys () {
-        return Object.keys(this._latches.map)
+        return Object.keys(this._futures.map)
     }
 
     // Get the value for the given `key` in the `Cubbyhole`. Returns a promise
     // that resolves to the value or rejects if the value is rejected.
-    async get(key) {
-        const latch = this._terminator || this._latches.get(key)
-        return await latch.promise
+    get(key) {
+        const future = this._terminator || this._futures.get(key)
+        return future.promise
     }
 
     // **TODO** Maybe we explicitly name this `resolve` and have a separate
     // explicit `reject`. Would make `destroy` require a separate definition,
     // `destroyAllResolve` or `destroyAllReject()`.
     // Set the value of the `key` to the given `value`.
-    set (key, value) {
-        if (!this.destroyed) {
-            this._latches.get(key).unlatch(value)
+    resolve (key, ...vargs) {
+        if (! this.destroyed) {
+            const future = this._futures.get(key)
+            future.resolve.apply(future, vargs)
+        }
+    }
+
+    reject (key, error) {
+        if (! this.destroyed) {
+            this._futures.get(key).reject(error)
         }
     }
 
     // Remove the entry for the given `key` from the `Cubbyhole`.
     remove (key) {
-        delete this._latches.map[key]
+        delete this._futures.map[key]
     }
 
     // Destroy the `Cubbyhole`.  If the given `value` is an `Error`, reject the
     // outstanding and future keys with the `Error`, otherwise resolve the
     // outstanding and future keys with the `value`. If no `value` is given the
     // outstanding and future keys are resolved with `null`.
-    destroy () {
-        const value = arguments.length == 0 ? null : arguments[0]
+    destroy (future = Future.resolve()) {
+        assert(future.fulfilled)
         this.destroyed = true
-        this._terminator = new Latch()
-        this._terminator.unlatch(value)
-        for (const key in this._latches.map) {
-            this._latches.map[key].unlatch(value)
-            delete this._latches.map[key]
+        this._terminator = future
+        for (const key in this._futures.map) {
+            if (future.rejection == null) {
+                this._futures.map[key].resolve.apply(this._futures.map[key], future.resolution)
+            } else {
+                this._futures.map[key].reject(future.rejection)
+            }
         }
     }
 }
--- test/cubbyhole.t.js
+++ test/cubbyhole.t.js
@@ -1,5 +1,6 @@
 require('proof')(13, async (okay) => {
     const Cubbyhole = require('..')
+    const Future = require('perhaps')
     {
         const cubbyhole = new Cubbyhole
         cubbyhole.destroy()
@@ -8,7 +9,7 @@ require('proof')(13, async (okay) => {
     {
         const cubbyhole = new Cubbyhole
         const got = cubbyhole.get('x')
-        cubbyhole.set('x', 1)
+        cubbyhole.resolve('x', 1)
         okay({
             got: await got,
             keys: cubbyhole.keys
@@ -19,7 +20,7 @@ require('proof')(13, async (okay) => {
     }
     {
         const cubbyhole = new Cubbyhole
-        cubbyhole.set('x', 1)
+        cubbyhole.resolve('x', 1)
         okay(cubbyhole.keys, [ 'x' ], 'keys before remove')
         cubbyhole.remove('x')
         okay(cubbyhole.keys, [], 'keys after removed')
@@ -27,7 +28,7 @@ require('proof')(13, async (okay) => {
     {
         const test = []
         const cubbyhole = new Cubbyhole
-        cubbyhole.set('x', new Error('reject'))
+        cubbyhole.reject('x', new Error('reject'))
         try {
             await cubbyhole.get('x')
         } catch (error) {
@@ -37,25 +38,30 @@ require('proof')(13, async (okay) => {
     }
     {
         const cubbyhole = new Cubbyhole
-        cubbyhole.set('x', 1)
+        cubbyhole.resolve('x', 1)
         okay(cubbyhole.keys, [ 'x' ], 'keys before destroyed')
         const promise = cubbyhole.get('y')
-        okay(cubbyhole.latched, [ 'y' ], 'latched before destroyed')
-        cubbyhole.destroy(2)
-        cubbyhole.set('x', 1)
-        okay(cubbyhole.latched, [], 'latched after destroyed')
-        okay(cubbyhole.keys, [], 'keys after destroyed')
+        okay(cubbyhole.unfulfilled, [ 'y' ], 'latched before destroyed')
+        cubbyhole.destroy(Future.resolve(2))
+        cubbyhole.resolve('x', 1)
+        okay(cubbyhole.unfulfilled, [], 'latched after destroyed')
+        okay(cubbyhole.keys, [ 'x', 'y' ], 'keys after destroyed')
         okay(await promise, 2, 'get when destroyed resolves to destruction value')
         okay(await cubbyhole.get('z'), 2, 'get after destroyed resolve to destruction value')
     }
     {
         const cubbyhole = new Cubbyhole
-        cubbyhole.destroy(new Error)
+        cubbyhole.destroy(Future.reject(new Error))
         okay('reject cancel when no one awaits the terminal error')
     }
     {
         const cubbyhole = new Cubbyhole
-        cubbyhole.set('x', new Error)
+        cubbyhole.reject('x', new Error)
         okay('reject a key when no one awaits the error')
+    }
+    {
+        const cubbyhole = new Cubbyhole
+        cubbyhole.reject('x', new Error)
+        await new Promise(resolve => setImmediate(resolve))
     }
 })
